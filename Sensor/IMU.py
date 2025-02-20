5fae236c8999aedd29fc75c852b870bb12d1b544
Heejun Park <parkie0517@gmail.com>
2025-02-20 18:59:52 +0900

    imu sensor

--- Sensor/IMU.py
+++ Sensor/IMU.py
@@ -6,8 +6,8 @@ import time
 from lib.network.UDP import Receiver
 from lib.define.IMU import IMU
 
-IP = '127.0.0.1' 
-PORT = 1111
+IP = '143.248.50.159' # ego info 받는 컴퓨터/서버의 ip주소
+PORT = 9091 # imu 센서의 destination port (시뮬에서 f3누르고 imu 세팅 들어가면 확인 가능)
 
 #Protocol 정보
 #https://help-morai-sim.scrollhelp.site/ko/morai-sim-drive/24.R2/ros-1#id-(24.R2-ko)통신메시지프로토콜-EgoVehicleStatus.1
